810d0a2907af5b876b40b9f35c4bf972092ca602
Aleksey Karev <aleksey.karev@raiffeisen.ru>
2024-03-09 21:36:34 +0100

    feat: add models and DTOs

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+
+@Data
+@AllArgsConstructor
 public class ItemDto {
+    private Integer id;
+    private String name;
+    private String description;
+    private Boolean available;
+
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable()
+        );
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    @NotNull
+    @Email
+    private String email;
+}
