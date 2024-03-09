2606c8285206d2704b4c6674f3a256b33a5a7b5e
Aleksey Karev <aleksey.karev@raiffeisen.ru>
2024-03-09 21:37:03 +0100

    feat: add controllers

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,12 +1,9 @@
-package ru.practicum.shareit.item;
+package ru.practicum.shareit.request.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
-@RequestMapping("/items")
-public class ItemController {
+@RequestMapping(path = "/requests")
+public class ItemRequestController {
 }
---
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -0,0 +1,53 @@
+package ru.practicum.shareit.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto create(@RequestBody @Valid User user) {
+        return UserDto.toUserDto(userService.create(user));
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto update(@RequestBody User user, @PathVariable Integer userId) {
+        return UserDto.toUserDto(userService.update(user, userId));
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable Integer userId) {
+        userService.delete(userId);
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getUserById(@PathVariable Integer userId) {
+        return UserDto.toUserDto(userService.getUserById(userId));
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers().stream()
+                .map(UserDto::toUserDto)
+                .collect(Collectors.toList());
+    }
+}
