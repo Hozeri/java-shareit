b6d90f56d26ce8cbb23e6dee9afc24c770f0e257
Aleksey Karev <aleksey.karev@raiffeisen.ru>
2024-03-09 21:40:57 +0100

    feat: add error handler and exceptions

---
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -0,0 +1,37 @@
+package ru.practicum.shareit.error;
+
+import ru.practicum.shareit.exception.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final RuntimeException e) {
+        log.error(e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        log.error(e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAnyException(final Throwable e) {
+        log.error(e.getMessage(), e);
+        return new ErrorResponse("Произошла непредвиденная ошибка.");
+    }
+}
