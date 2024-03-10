256919b301b5c9e6e3a1c6d06dfe920825043c5a
Aleksey Karev <aleksey.karev@raiffeisen.ru>
2024-03-10 21:41:45 +0100

    feat: add item controller

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,14 +1,59 @@
 package ru.practicum.shareit.item.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.error.ErrorHandler;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping("/items")
+@RequiredArgsConstructor
 public class ItemController {
 
+    private final ItemService itemService;
+    private final ErrorHandler errorHandler;
+    private static final String USER_ID_HEADER = "X-Sharer-User-Id";
 
+    @PostMapping
+    public ItemDto addNewItem(@RequestHeader(USER_ID_HEADER) Integer userId,
+                              @RequestBody @Valid ItemDto itemDto,
+                              BindingResult bindingResult) {
+        errorHandler.throwValidationException(bindingResult);
+        return itemService.addNewItem(userId, itemDto);
+    }
+
+    @PatchMapping("/{itemDtoId}")
+    public ItemDto updateItem(@RequestHeader(USER_ID_HEADER) Integer userId,
+                              @PathVariable Integer itemDtoId,
+                              @RequestBody ItemDto itemDto) {
+        return itemService.updateItem(userId, itemDtoId, itemDto);
+    }
+
+    @GetMapping("/{itemDtoId}")
+    public ItemDto getItemById(@PathVariable Integer itemDtoId) {
+        return itemService.getItemById(itemDtoId);
+    }
+
+    @GetMapping
+    public List<ItemDto> getItemsByUserId(@RequestHeader(USER_ID_HEADER) Integer userId) {
+        return itemService.getAllItemDtosByUserId(userId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> search(@RequestParam String text) {
+        return itemService.searchItemDtosByText(text);
+    }
 }
