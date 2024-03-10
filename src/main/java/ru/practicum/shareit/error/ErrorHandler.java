e5700773966e08df41fae60eda147f63ce7e7eb0
Aleksey Karev <aleksey.karev@raiffeisen.ru>
2024-03-10 21:38:26 +0100

    feat: add validation exception handler

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -1,15 +1,17 @@
 package ru.practicum.shareit.error;
 
-import ru.practicum.shareit.exception.EmailDuplicateException;
-import ru.practicum.shareit.exception.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exception.EntityNotFoundException;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -22,16 +24,9 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler({ConstraintViolationException.class})
+    @ExceptionHandler({ValidationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
-        log.error(e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler({EmailDuplicateException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleEmailDuplicateException(final RuntimeException e) {
         log.error(e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
@@ -41,5 +36,15 @@ public class ErrorHandler {
     public ErrorResponse handleAnyException(final Throwable e) {
         log.error(e.getMessage(), e);
         return new ErrorResponse("Произошла непредвиденная ошибка.");
+    }
+
+    public void throwValidationException(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            throw new ValidationException(String.join(",", errors));
+        }
     }
 }
